b9be57b40fad449c0a1e127b6b0af44653bba541
brayan.coy <brayan.coy@latinoaustralia.com>
2024-05-08 16:39:20 -0400

    fix heroku

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,4 +1,4 @@
-export const truncateAddress = (address) => {
+export const truncateAddress = (address: string) => {
     if (!address) return "No Account";
     const match = address.match(
       /^(0x[a-zA-Z0-9]{2})[a-zA-Z0-9]+([a-zA-Z0-9]{2})$/
@@ -7,7 +7,7 @@ export const truncateAddress = (address) => {
     return `${match[1]}…${match[2]}`;
   };
   
-  export const toHex = (num) => {
+  export const toHex = (num: any) => {
     const val = Number(num);
     return "0x" + val.toString(16);
   };
